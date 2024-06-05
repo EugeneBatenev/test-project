3c804588fbfd6512ca23afbf1ed3719654724bd9
EugeneBatenev <nyashenator@gmail.com>
2024-06-05 14:35:48 +0000

    yoy

---
+++ test/sampleTest.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('Sample Test Suite', function() {
+    it('should return true', function() {
+        expect(true).to.be.true;
+    });
+
+    it('should return false', function() {
+        expect(false).to.be.false;
+    });
+});
